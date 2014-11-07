992712f3bebd3809e0f14f8e30e47188161a7126
RazvanGirmacea <girmacearazvan@gmail.com>
2014-11-07 17:58:56 +0200

    implement datepicker with selectboxes (original html code)

--- pickdate/pickdate.js
+++ pickdate/pickdate.js
@@ -1,17 +1,68 @@
 $('document').ready(function() {
-    $('#L_E_date, #L_P_date').datepicker({
-        showOn: 'both',
-        buttonImageOnly: true,
-        buttonImage: "pickadate_icon.jpg",
-
-        dateFormat: 'M d, yy',
-        altFormat: 'm/d/yy',
+    // L_P_date
+    $('#L_E_date').datepicker({
+        dateFormat: 'm/d/yy',
+        minDate: '1/1/2007',
+        maxDate: '12/31/' + (new Date().getFullYear() + 2),
 
         showButtonPanel: true,
         changeMonth: true,
-        changeYear: true
+        changeYear: true,
+
+        onSelect: function(date) {
+            var arr = date.split('/');
+            var month = arr[0];
+            var day = arr[1];
+            var year = arr[2];
+
+            $('#L_E_month').val(month);
+            $('#L_E_day').val(day);
+            $('#L_E_year').val(year);
+        }
     });
 
-    $('#L_E_date').datepicker('option', 'altField', '#L_E_alt');
-    $('#L_P_date').datepicker('option', 'altField', '#L_P_alt');
+    $('#L_E_day, #L_E_month, #L_E_year').on('change', function(){
+       $('#L_E_date').datepicker('setDate', makeDate('E'));
+    });
+
+    $('#L_E_datepicker').on('click', function() {
+       $('#L_E_date').datepicker('show');
+    });
+
+
+
+    // L_P_date
+    $('#L_P_date').datepicker({
+        dateFormat: 'm/d/yy',
+        minDate: '1/1/2007',
+        maxDate: '12/31/' + (new Date().getFullYear() + 2),
+
+        showButtonPanel: true,
+        changeMonth: true,
+        changeYear: true,
+
+        onSelect: function(date) {
+            var arr = date.split('/');
+            var month = arr[0];
+            var day = arr[1];
+            var year = arr[2];
+
+            $('#L_P_month').val(month);
+            $('#L_P_day').val(day);
+            $('#L_P_year').val(year);
+        }
+    });
+
+    $('#L_P_day, #L_P_month, #L_P_year').on('change', function(){
+        $('#L_P_date').datepicker('setDate', makeDate('P'));
+    });
+
+    $('#L_P_datepicker').on('click', function() {
+        $('#L_P_date').datepicker('show');
+    });
+
+
+    var makeDate = function(letter) {
+        return $('#L_' + letter + '_month').val() + '/' + $('#L_' + letter + '_day').val() + '/' + $('#L_' + letter + '_year').val();
+    }
 });
--- pickdate/pickdate.php
+++ pickdate/pickdate.php
@@ -21,13 +21,46 @@ $L_P_year  = 2014;
                 <td height='22' bgcolor="#EDEFF4" colspan="2">Date1:</td>
                 <td height='22' bgcolor="#EDEFF4">
 
-                    <input type="text"
-                           id="L_E_date"
-                           class="textboxx"
-                           value="<?= date('M d, Y', mktime(null, null, null, $L_E_month, $L_E_day, $L_E_year)) ?>">
+                    <?php
+                    echo "<select name='L_E_month' id='L_E_month' class='textboxx'  tabindex='41'>";
+                    echo "<option value=''></option>";
+                    if ($L_E_month==1){echo "<option value=1 selected>Jan</option>";}else{echo "<option value=1>Jan</option>";}
+                    if ($L_E_month==2){echo "<option value=2 selected>Feb</option>";}else{echo "<option value=2>Feb</option>";}
+                    if ($L_E_month==3){echo "<option value=3 selected>Mar</option>";}else{echo "<option value=3>Mar</option>";}
+                    if ($L_E_month==4){echo "<option value=4 selected>Apr</option>";}else{echo "<option value=4>Apr</option>";}
+                    if ($L_E_month==5){echo "<option value=5 selected>May</option>";}else{echo "<option value=5>May</option>";}
+                    if ($L_E_month==6){echo "<option value=6 selected>Jun</option>";}else{echo "<option value=6>Jun</option>";}
+                    if ($L_E_month==7){echo "<option value=7 selected>Jul</option>";}else{echo "<option value=7>Jul</option>";}
+                    if ($L_E_month==8){echo "<option value=8 selected>Aug</option>";}else{echo "<option value=8>Aug</option>";}
+                    if ($L_E_month==9){echo "<option value=9 selected>Sep</option>";}else{echo "<option value=9>Sep</option>";}
+                    if ($L_E_month==10){echo "<option value=10 selected>Oct</option>";}else{echo "<option value=10>Oct</option>";}
+                    if ($L_E_month==11){echo "<option value=11 selected>Nov</option>";}else{echo "<option value=11>Nov</option>";}
+                    if ($L_E_month==12){echo "<option value=12 selected>Dec</option>";}else{echo "<option value=12>Dec</option>";}
+                    echo "</select>";
+
+                    echo "<select name='L_E_day' id='L_E_day' class='textboxx'  tabindex='42'>";
+                    echo "<option value=''></option>";
+                    for ($i = 1; $i <= 31; $i++) {
+                        if ($L_E_day==$i){echo "<option value=".$i." selected>".$i."</option>";}else{echo "<option value=".$i.">".$i."</option>";}
+                    }
+                    echo "</select>";
+
+                    echo "<select name='L_E_year' id='L_E_year' class='textboxx'  tabindex='43'>";
+                    echo "<option value=''></option>";
+                    $this_year_plus2=date(Y)+2;
+                    for ($i = 2007; $i <= $this_year_plus2; $i++) {
+                        if ($L_E_year==$i){echo "<option value=".$i." selected>".$i."</option>";}else{echo "<option value=".$i.">".$i."</option>";}
+                    }
+                    echo "</select>";
+                    ?>
+
                     <input type="hidden"
-                           id="L_E_alt"
+                           id="L_E_date"
+                           name="L_E_date"
+                           class="textboxx"
                            value="<?= date('m/d/Y', mktime(null, null, null, $L_E_month, $L_E_day, $L_E_year)) ?>">
+
+                    <img src="pickadate_icon.jpg" id="L_E_datepicker">
                 </td>
             </tr>
 
@@ -38,13 +71,47 @@ $L_P_year  = 2014;
                 <td height='22' bgcolor="#EDEFF4" colspan="2">Date2:</td>
                 <td height='22' bgcolor="#EDEFF4">
 
-                    <input type="text"
-                           id="L_P_date"
-                           class="textboxx"
-                           value="<?= date('M d, Y', mktime(null, null, null, $L_P_month, $L_P_day, $L_P_year)) ?>">
+                    <?php
+                    echo "<select name='L_P_month' id='L_P_month' class='textboxx'  tabindex='41'>";
+                    echo "<option value=''></option>";
+                    if ($L_E_month==1){echo "<option value=1 selected>Jan</option>";}else{echo "<option value=1>Jan</option>";}
+                    if ($L_E_month==2){echo "<option value=2 selected>Feb</option>";}else{echo "<option value=2>Feb</option>";}
+                    if ($L_E_month==3){echo "<option value=3 selected>Mar</option>";}else{echo "<option value=3>Mar</option>";}
+                    if ($L_E_month==4){echo "<option value=4 selected>Apr</option>";}else{echo "<option value=4>Apr</option>";}
+                    if ($L_E_month==5){echo "<option value=5 selected>May</option>";}else{echo "<option value=5>May</option>";}
+                    if ($L_E_month==6){echo "<option value=6 selected>Jun</option>";}else{echo "<option value=6>Jun</option>";}
+                    if ($L_E_month==7){echo "<option value=7 selected>Jul</option>";}else{echo "<option value=7>Jul</option>";}
+                    if ($L_E_month==8){echo "<option value=8 selected>Aug</option>";}else{echo "<option value=8>Aug</option>";}
+                    if ($L_E_month==9){echo "<option value=9 selected>Sep</option>";}else{echo "<option value=9>Sep</option>";}
+                    if ($L_E_month==10){echo "<option value=10 selected>Oct</option>";}else{echo "<option value=10>Oct</option>";}
+                    if ($L_E_month==11){echo "<option value=11 selected>Nov</option>";}else{echo "<option value=11>Nov</option>";}
+                    if ($L_E_month==12){echo "<option value=12 selected>Dec</option>";}else{echo "<option value=12>Dec</option>";}
+                    echo "</select>";
+
+                    echo "<select name='L_P_day' id='L_P_day' class='textboxx'  tabindex='42'>";
+                    echo "<option value=''></option>";
+                    for ($i = 1; $i <= 31; $i++) {
+                        if ($L_E_day==$i){echo "<option value=".$i." selected>".$i."</option>";}else{echo "<option value=".$i.">".$i."</option>";}
+                    }
+                    echo "</select>";
+
+                    echo "<select name='L_P_year' id='L_P_year' class='textboxx'  tabindex='43'>";
+                    echo "<option value=''></option>";
+                    $this_year_plus2=date(Y)+2;
+                    for ($i = 2007; $i <= $this_year_plus2; $i++) {
+                        if ($L_E_year==$i){echo "<option value=".$i." selected>".$i."</option>";}else{echo "<option value=".$i.">".$i."</option>";}
+                    }
+                    echo "</select>";
+                    ?>
+
                     <input type="hidden"
-                           id="L_P_alt"
+                           id="L_P_date"
+                           name="L_P_date"
+                           class="textboxx"
                            value="<?= date('m/d/Y', mktime(null, null, null, $L_P_month, $L_P_day, $L_P_year)) ?>">
+
+                    <img src="pickadate_icon.jpg" id="L_P_datepicker">
+
                 </td>
             </tr>
         </table>
